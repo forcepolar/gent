d248a5959eafc7a83a7b4ddd0b7651d27f262783
forcepolar <animator4sr@gmail.com>
2025-02-23 06:28:53 +0300

    Update g3.sh

--- g3.sh
+++ g3.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 set -e
 
-# Проверка, что скрипт запущен от root
+# Proverka, chto skript zapushchen ot root
 if [ "$(id -u)" -ne 0 ]; then
-    echo "Этот скрипт должен быть запущен от root"
+    echo "Etot skript dolzhen byt' zapushchen ot root"
     exit 1
 fi
 
-# Проверка наличия whiptail (если нет, устанавливаем dialog)
+# Proverka nalichiya whiptail (esli net, ustanavlivaem dialog)
 if ! command -v whiptail &> /dev/null; then
-    echo "Устанавливаем dialog..."
+    echo "Ustanavlivaem dialog..."
     pacman -Sy --noconfirm dialog
     alias whiptail=dialog
 fi
 
-# Выбор диска
-DISK=$(whiptail --inputbox "Введите диск для установки (по умолчанию /dev/sdb):" 10 60 /dev/sdb 3>&1 1>&2 2>&3)
+# Vybor diska
+DISK=$(whiptail --inputbox "Vvedite disk dlya ustanovki (po umolchaniyu /dev/sdb):" 10 60 /dev/sdb 3>&1 1>&2 2>&3)
 if [ -z "$DISK" ]; then
     DISK="/dev/sdb"
 fi
 
-# Подтверждение разметки диска
-if whiptail --yesno "Разметить диск автоматически?" 10 60; then
+# Podtverzhdenie razmetki diska
+if whiptail --yesno "Razmetit' disk avtonomno?" 10 60; then
     parted "$DISK" mklabel gpt
     parted "$DISK" mkpart ESP fat32 1MiB 512MiB
     parted "$DISK" set 1 boot on
@@ -32,46 +32,46 @@ if whiptail --yesno "Разметить диск автоматически?" 10
     mkfs.ext4 "${DISK}3"
 fi
 
-# Монтирование разделов
+# Montirovanie razdelov
 mount "${DISK}3" /mnt
 mkdir -p /mnt/boot
 mount "${DISK}1" /mnt/boot
 
-# Загрузка Stage3
+# Zagruska Stage3
 cd /mnt
 STAGE3_FILE="stage3-amd64-systemd-latest.tar.xz"
-echo "Загрузка $STAGE3_FILE..."
+echo "Zagruska $STAGE3_FILE..."
 curl -O "https://mirror.yandex.ru/gentoo-distfiles/$STAGE3_FILE"
 tar xpvf "$STAGE3_FILE" --xattrs-include='*.*' --numeric-owner
 
-# Настройка chroot
+# Nastroika chroot
 cp -L /etc/resolv.conf /mnt/etc/
 mount -t proc /proc /mnt/proc
 mount --rbind /sys /mnt/sys
 mount --rbind /dev /mnt/dev
 
-# Передача переменной DISK внутрь chroot
+# Peredacha peremennoy DISK vnutr' chroot
 echo "DISK=${DISK}" > /mnt/root/install_disk
 
-# Вход в chroot и выполнение дальнейшей установки
+# Vkhod v chroot i vypolnenie dal'neyshey ustanovki
 chroot /mnt /bin/bash <<'EOF'
 source /etc/profile
 export PS1="(chroot) \$PS1"
 
-# Импорт переменной DISK
+# Import peremennoy DISK
 if [ -f /root/install_disk ]; then
     source /root/install_disk
 else
-    echo "Не удалось импортировать переменную DISK"
+    echo "Ne udalos' importirovat' peremennuyu DISK"
     exit 1
 fi
 
-# Обновление портежей и системы
+# Obnovlenie portezev i sistemy
 emerge-webrsync
 emerge --sync
 emerge -avuDN @world
 
-# Настройка времени и локали
+# Nastroika vremeni i lokaley
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 echo "UTC" > /etc/timezone
 emerge --config sys-libs/timezone-data
@@ -80,17 +80,17 @@ echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
 echo 'LANG="en_US.UTF-8"' > /etc/locale.conf
 
-# Настройка безопасности паролей
+# Nastroika bezopasnosti paroly
 echo "min=1,1,1,1,1" > /etc/security/passwdqc.conf
 
-# Настройка hostname и сети
+# Nastroika hostname i seti
 echo "halaxygentoo" > /etc/hostname
 emerge -av systemd-networkd
 systemctl enable systemd-networkd systemd-resolved
 cp -r /etc/systemd/network /mnt/etc/systemd/network
 ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
 
-# Установка и компиляция ядра
+# Ustanovka i kompilyatsiya yadra
 emerge -av sys-kernel/gentoo-sources
 cd /usr/src/linux
 make menuconfig
@@ -98,40 +98,5 @@ make -j$(nproc)
 make modules_install
 make install
 
-# Выбор загрузчика
-if whiptail --yesno "Хотите использовать GRUB вместо systemd-boot?" 10 60; then
-    emerge -av grub
-    grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-    grub-mkconfig -o /boot/grub/grub.cfg
-else
-    bootctl install
-    echo -e "title Gentoo Linux\nlinux /vmlinuz\ninitrd /boot/initramfs\noptions root=${DISK}3 rw" > /boot/loader/entries/gentoo.conf
-fi
-
-# Установка Xorg, bspwm и драйверов NVIDIA
-echo "Установка Xorg, bspwm и NVIDIA..."
-emerge -av xorg-server xorg-xinit bspwm sxhkd dmenu alacritty nvidia-drivers
-mkdir -p /home/ervin
-echo "exec bspwm" > /home/ervin/.xinitrc
-chown ervin:ervin /home/ervin/.xinitrc
-
-# Установка пароля для root
-whiptail --msgbox "Установите пароль для root" 10 60
-passwd
-
-# Установка пользователя ervin
-useradd -m -G wheel -s /bin/bash ervin
-whiptail --msgbox "Установите пароль для пользователя ervin" 10 60
-passwd ervin
-echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
-
-exit
-EOF
-
-# Завершение установки
-umount -l /mnt/boot
-umount -l /mnt/sys
-umount -l /mnt/proc
-umount -l /mnt/dev
-swapoff "${DISK}2"
-reboot
+# Vybor zagruzchika
+if whiptail --yesno "Khotite ispol'zovat' GRUB vmesto systemd-boot?" 10 60
