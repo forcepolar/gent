04aee284d3543f31cdb8db6173f33ca4d71ab527
forcepolar <animator4sr@gmail.com>
2025-02-24 04:47:49 +0300

    4:45

--- g1.sh
+++ g1.sh
@@ -111,7 +111,6 @@ mount --rbind /dev /mnt/gentoo/dev
 chroot /mnt/gentoo /bin/bash << 'EOF'
 
 # Шаг 8: Обновление системы и установка пакетов
-<<<<<<< HEAD
 echo -e "\n\033[1;32m[8/20] Обновление системы и установка пакетов\033[0m"
 emerge-webrsync
 emerge --update --deep --newuse @world
@@ -159,67 +158,11 @@ echo "$USERNAME:$PASSWORD" | chpasswd
 
 # Шаг 14: Настройка локали
 echo -e "\n\033[1;32m[14/20] Настройка локали\033[0m"
-=======
-echo -e "\n\033[1;32m[8/14] Обновление системы и установка пакетов\033[0m"
-emerge-webrsync
-echo "Выберите пакеты для установки:"
-echo "1. sys-boot/grub"
-echo "2. sys-kernel/gentoo-sources"
-echo "3. sys-apps/systemd"
-echo "4. sys-fs/e2fsprogs"
-echo "5. sys-fs/udev"
-echo "6. sys-fs/udev-init-scripts"
-read -p "Введите номера пакетов для установки (разделенные пробелами): " PACKAGES
-emerge --update --deep --newuse $PACKAGES
-
-# Шаг 9: Настройка fstab
-echo -e "\n\033[1;32m[9/14] Настройка fstab\033[0m"
-echo "/dev/sda3 / ext4 defaults 0 1" >> /etc/fstab
-echo "/dev/sda2 none swap sw 0 0" >> /etc/fstab
-echo "/dev/sda1 /boot/efi vfat defaults 0 1" >> /etc/fstab
-
-# Шаг 10: Установка и настройка bootloaderа
-echo -e "\n\033[1;32m[10/14] Установка и настройка GRUB\033[0m"
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
-
-# Шаг 11: Настройка часового пояса
-echo -e "\n\033[1;32m[11/14] Настройка часового пояса\033[0m"
-echo "Выберите часовой пояс:"
-echo "1. Москва"
-echo "2. Киев"
-echo "3. Минск"
-read -p "Введите номер часового пояса: " TIMEZONE
-case $TIMEZONE in
-    1)
-        ln -sf /usr/share/zoneinfo/Moscow /etc/localtime
-        ;;
-    2)
-        ln -sf /usr/share/zoneinfo/Kiev /etc/localtime
-        ;;
-    3)
-        ln -sf /usr/share/zoneinfo/Minsk /etc/localtime
-        ;;
-    *)
-        echo "Неверный выбор!" && exit 1
-        ;;
-esac
-hwclock --systohc
-
-# Шаг 12: Настройка имени хоста
-echo -e "\n\033[1;32m[12/14] Настройка имени хоста\033[0m"
-read -p "Введите имя хоста: " HOSTNAME
-echo "$HOSTNAME" > /etc/hostname
-
-# Шаг 13: Настройка локали
-echo -e "\n\033[1;32m[13/14] Настройка локали\033[0m"
->>>>>>> ff42552f1cc7db24aeae1bd626833484b73fa1e7
 echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 eselect locale set ru_RU.UTF-8
 env-update && source /etc/profile
 
-<<<<<<< HEAD
 # Шаг 15: Настройка часового пояса
 echo -e "\n\033[1;32m[15/20] Настройка часового пояса\033[0m"
 echo "Введите часовой пояс: "
@@ -244,26 +187,7 @@ service iptables restart
 
 # Шаг 18: Настройка SELinux
 echo -e "\n\033[1;32m[18/20] Настройка SELinux\033[0m"
-emerge --update --deep --newuse sec-policy/selinux-base
-selinux-activate
-
 # Шаг 19: Настройка системы
 echo -e "\n
-=======
-# Шаг 14: Выход из chroot и очистка
-echo -e "\n\033[1;32m[14/14] Выход из chroot и очистка\033[0m"
-exit
-
-EOF
-
-# Размонтирование
-echo -e "\nРазмонтирование файловых систем..."
-umount -R /mnt/gentoo/dev
-umount -R /mnt/gentoo/sys
-umount -R /mnt/gentoo/proc
-umount /mnt/gentoo/boot/efi
-umount /mnt/gentoo
-
-# Последнее сообщение
-echo -e "\n\033[1;32mУстановка завершена! Теперь вы можете перезагрузить систему.\033[0m"
->>>>>>> ff42552f1cc7db24aeae1bd626833484b73fa1e7
+emerge --update --deep --newuse sec-policy/selinux-base
+selinux-activate
